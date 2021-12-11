775df7a1921cec595580ad7ee45dca9c8cc2c4d6
smori <shinichiro.mori.1983@gmail.com>
2021-12-11 14:47:13 +0900

    Move invocation of go() to form submit event

--- src/SearchBox.vue
+++ src/SearchBox.vue
@@ -1,20 +1,21 @@
 <template>
   <div class="search-box">
-    <input
-      @input="query = $event.target.value"
-      aria-label="Search"
-      :value="query"
-      :class="{ focused: focused }"
-      :placeholder="placeholder"
-      autocomplete="off"
-      spellcheck="false"
-      @focus="focused = true"
-      @blur="focused = false"
-      @keyup.enter="go(focusIndex)"
-      @keyup.up="onUp"
-      @keyup.down="onDown"
-      ref="input"
-    />
+    <form @submit.prevent="go(focusIndex)">
+      <input
+        @input="query = $event.target.value"
+        aria-label="Search"
+        :value="query"
+        :class="{ focused: focused }"
+        :placeholder="placeholder"
+        autocomplete="off"
+        spellcheck="false"
+        @focus="focused = true"
+        @blur="focused = false"
+        @keyup.up="onUp"
+        @keyup.down="onDown"
+        ref="input"
+      />
+    </form>
     <ul
       class="suggestions"
       v-if="showSuggestions"
